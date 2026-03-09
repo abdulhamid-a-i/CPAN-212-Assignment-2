238e94308e7db40a1972044d66ffac16cbf8d9d5
Abdulhamid <abdul.ali.weheliye@gmail.com>
2026-03-09 04:47:10 -0400

    Changed app.js

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ app.use(sessionConfig);
 app.use("/api/auth", authRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/requests", requestRoutes);
-app.use("/api/quotes", quoteRoutes);
+app.use("/api", quoteRoutes);
 
 app.use(errorHandler);
 
