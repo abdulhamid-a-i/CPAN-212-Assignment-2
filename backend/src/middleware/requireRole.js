6e9e35df6c4b20dd9a1e47a7e65171df861c3aff
Abdulhamid <abdul.ali.weheliye@gmail.com>
2026-03-15 00:53:19 -0400

    Removed debugging related code

--- backend/src/middleware/requireRole.js
+++ backend/src/middleware/requireRole.js
@@ -15,24 +15,17 @@ export const requireRole = (role) => {
 export const requireRequestOwnership = async (req, res, next) => {
   try {
     const { id } = req.params;
-    console.log("Params:", req.params);
-console.log("ID:", req.params.id);
-    console.log("Incoming id:", id);
 
     if (!mongoose.Types.ObjectId.isValid(id)) {
       return next(new AppError("Forbidden", 403));
     }
-    console.log("id valid")
 
     const request = await ServiceRequest.findById(id);
-    console.log(request);
 
     if(!request){
       return next(new AppError("Request Not Found", 404));
     }
 
-    console.log("Request Ownder: "+request.createdBy)
-    console.log("User: "+ req.session.userId)
     const isOwner = request.createdBy.toString() === req.session.userId;
 
     const isResident = req.session.role === 'resident'
