7c32a9ec46b75c7460b1157b4702844dbf60d458
Abdulhamid <abdul.ali.weheliye@gmail.com>
2026-03-15 03:23:03 -0400

    Removed username badge

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,7 +2,6 @@ import { Component, inject } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AuthService } from './services/auth.service';
 import { CommonModule } from '@angular/common';
-import { User } from './models/user.model';
 
 @Component({
   selector: 'app-root',
@@ -15,31 +14,10 @@ export class App {
   authService = inject(AuthService);
   router = inject(Router);
 
-  currentUser: User | null = null;
-
-  ngOnInit(): void {
-    this.loadCurrentUser();
-  }
-  
   async logout(): Promise<void> {
     await this.authService.logout();
     this.router.navigate(['/login']);
   }
 
-  loadCurrentUser(): void {
-    this.authService.me().subscribe({
-      next: (res) => {
-        this.currentUser = res.user;
-        console.log(this.currentUser.role)
-      },
-      error: () => {
-        return null
-      }
-    });
-  }
 
-  get UserName(): string {
-    if (!this.currentUser) return '';
-    return this.currentUser.fullName;
-  }
 }
