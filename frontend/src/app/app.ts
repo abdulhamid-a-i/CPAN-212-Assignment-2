d059c13cbb28079fe9ccf911ec1dca4c8585d853
Abdulhamid <abdul.ali.weheliye@gmail.com>
2026-03-15 02:52:35 -0400

    Navbar UI improvements

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AuthService } from './services/auth.service';
 import { CommonModule } from '@angular/common';
+import { User } from './models/user.model';
 
 @Component({
   selector: 'app-root',
@@ -13,9 +14,32 @@ import { CommonModule } from '@angular/common';
 export class App {
   authService = inject(AuthService);
   router = inject(Router);
+
+  currentUser: User | null = null;
+
+  ngOnInit(): void {
+    this.loadCurrentUser();
+  }
   
   async logout(): Promise<void> {
     await this.authService.logout();
     this.router.navigate(['/login']);
+  }
+
+  loadCurrentUser(): void {
+    this.authService.me().subscribe({
+      next: (res) => {
+        this.currentUser = res.user;
+        console.log(this.currentUser.role)
+      },
+      error: () => {
+        return null
+      }
+    });
+  }
+
+  get UserName(): string {
+    if (!this.currentUser) return '';
+    return this.currentUser.fullName;
   }
 }
--- frontend/src/app/pages/create-request/create-request.component.ts
+++ frontend/src/app/pages/create-request/create-request.component.ts
@@ -1,9 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
-
+import { AuthService } from '../../services/auth.service';
 import { RequestFormComponent } from '../../components/request-form/request-form.component';
 import { RequestService } from '../../services/request.service';
+import { User } from '../../models/user.model';
+
 
 @Component({
   selector: 'app-create-request',
@@ -15,9 +17,18 @@ import { RequestService } from '../../services/request.service';
 export class CreateRequestComponent {
   private requestService = inject(RequestService);
   private router = inject(Router);
+  private authService = inject(AuthService);
 
   successMessage = '';
   errorMessage = '';
+  currentUser: User | null = null;
+
+  ngOnInit(): void {
+    this.loadCurrentUser();
+    if (this.currentUser?.role !== "resident"){
+      this.router.navigate(['/requests']);
+    }
+  }
 
   handleSubmit(payload: {
     title: string;
@@ -34,6 +45,18 @@ export class CreateRequestComponent {
       error: (err) => {
         this.errorMessage = err?.error?.message || 'Failed to create request.';
         this.successMessage = '';
+      }
+    });
+  }
+
+    loadCurrentUser(): void {
+    this.authService.me().subscribe({
+      next: (res) => {
+        this.currentUser = res.user;
+        console.log(this.currentUser.role)
+      },
+      error: () => {
+        this.errorMessage = 'Unable to load current user.';
       }
     });
   }
